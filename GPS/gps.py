a8b321f32cf833806056b654ff15fecde04dcd38
MJCobar <maxwell.cobar@gmail.com>
2021-07-31 16:59:28 +0100

    Fixed the gps.py to work on FASTLogger

--- GPS/gps.py
+++ GPS/gps.py
@@ -1,5 +1,5 @@
-import navio.util
-import navio.ublox
+import Util.util
+import GPS.ublox as UBLX
 import numpy as np
 
 class GPS():
@@ -8,40 +8,40 @@ class GPS():
         self.longitude = -99
         self.altitude = -99
     def initialize(self):
-        self.ubl = navio.ublox.UBlox("spi:0.0", baudrate=5000000, timeout=2)
+        self.ubl = UBLX.UBlox("spi:0.0", baudrate=5000000, timeout=2)
 
         self.ubl.configure_poll_port()
-        self.ubl.configure_poll(navio.ublox.CLASS_CFG, navio.ublox.MSG_CFG_USB)
-        #ubl.configure_poll(navio.ublox.CLASS_MON, navio.ublox.MSG_MON_HW)
+        self.ubl.configure_poll(UBLX.CLASS_CFG, UBLX.MSG_CFG_USB)
+        #ubl.configure_poll(UBLX.CLASS_MON, UBLX.MSG_MON_HW)
 
-        self.ubl.configure_port(port=navio.ublox.PORT_SERIAL1, inMask=1, outMask=0)
-        self.ubl.configure_port(port=navio.ublox.PORT_USB, inMask=1, outMask=1)
-        self.ubl.configure_port(port=navio.ublox.PORT_SERIAL2, inMask=1, outMask=0)
+        self.ubl.configure_port(port=UBLX.PORT_SERIAL1, inMask=1, outMask=0)
+        self.ubl.configure_port(port=UBLX.PORT_USB, inMask=1, outMask=1)
+        self.ubl.configure_port(port=UBLX.PORT_SERIAL2, inMask=1, outMask=0)
         self.ubl.configure_poll_port()
-        self.ubl.configure_poll_port(navio.ublox.PORT_SERIAL1)
-        self.ubl.configure_poll_port(navio.ublox.PORT_SERIAL2)
-        self.ubl.configure_poll_port(navio.ublox.PORT_USB)
+        self.ubl.configure_poll_port(UBLX.PORT_SERIAL1)
+        self.ubl.configure_poll_port(UBLX.PORT_SERIAL2)
+        self.ubl.configure_poll_port(UBLX.PORT_USB)
         self.ubl.configure_solution_rate(rate_ms=1000)
 
         self.ubl.set_preferred_dynamic_model(None)
         self.ubl.set_preferred_usePPP(None)
         
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_POSLLH, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_PVT, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_STATUS, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_SOL, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_VELNED, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_SVINFO, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_VELECEF, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_POSECEF, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_RXM, navio.ublox.MSG_RXM_RAW, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_RXM, navio.ublox.MSG_RXM_SFRB, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_RXM, navio.ublox.MSG_RXM_SVSI, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_RXM, navio.ublox.MSG_RXM_ALM, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_RXM, navio.ublox.MSG_RXM_EPH, 1)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_TIMEGPS, 5)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_CLOCK, 5)
-        self.ubl.configure_message_rate(navio.ublox.CLASS_NAV, navio.ublox.MSG_NAV_DGPS, 5)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_POSLLH, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_PVT, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_STATUS, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_SOL, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_VELNED, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_SVINFO, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_VELECEF, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_POSECEF, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_RXM, UBLX.MSG_RXM_RAW, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_RXM, UBLX.MSG_RXM_SFRB, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_RXM, UBLX.MSG_RXM_SVSI, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_RXM, UBLX.MSG_RXM_ALM, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_RXM, UBLX.MSG_RXM_EPH, 1)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_TIMEGPS, 5)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_CLOCK, 5)
+        self.ubl.configure_message_rate(UBLX.CLASS_NAV, UBLX.MSG_NAV_DGPS, 5)
 
     def getfloat(self,instr):
         #print(instr)
@@ -72,7 +72,7 @@ class GPS():
         if msg is None:
             if opts.reopen:
                 self.ubl.close()
-                self.ubl = navio.ublox.UBlox("spi:0.0", baudrate=5000000, timeout=2)
+                self.ubl = UBLX.UBlox("spi:0.0", baudrate=5000000, timeout=2)
             return
         #print(msg.name())
         if msg.name() == "NAV_POSLLH":
